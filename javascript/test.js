db8bd6ad2d65637fc08dbcde4267b937af80bd8b
Gangadharan <626417@cognizant.com>
2017-11-07 15:05:47 +0530

    sdds

--- javascript/test.js
+++ javascript/test.js
@@ -1,7 +1,8 @@
 function login(){
-alert("login");
-alert("djfhwi");
-
+//alert("login");
+//alert("djfhwi");
+//var url="file:///D:/626417/myGit/myfirstgit/myfirstgit/html/2nd_page.html"
+window.location.href='file:///D:/626417/myGit/myfirstgit/myfirstgit/html/2nd_page.html';
 
 
 };
